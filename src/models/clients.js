24045eee0f83c39f872e83f387ee752daece1e40
marianavns <mariana.vns@gmail.com>
2020-11-01 19:52:07 -0300

    Cria o esqueleto dos futuros dados

---
+++ src/models/clients.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const clientsSchema = new mongoose.Schema(
+    {
+        email : { type : String},
+        cpf: { type: Number },
+        dataNascimento: { type: String },
+        estadoCivil: { type: String },
+        nomeColaboradora: { type: String },
+        telefone: { type: Number },
+        comprou: { type: Boolean },
+    },
+    {
+        versionKey: false
+    }
+)
+
+// Definindo a collection que será criada
+const clientsCollection = mongoose.model('clients', clientsSchema);
+
+// exportar o model para ser utilizado
+module.exports = clientsCollection;
